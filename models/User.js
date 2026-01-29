ed2ce68522c1e2ed04bf1c6cb0624f00a02847f8
RSCCV <115564033+RSCCV@users.noreply.github.com>
2026-01-29 16:55:53 +0100

    fix typo

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true, unique: true },
   fields: [{type: String}],
-  acessLevel: {type: String}
+  accessLevel: {type: String}
 });
 
 module.exports = mongoose.model('User', userSchema, 'users');
